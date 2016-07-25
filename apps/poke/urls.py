994d6622a1106264e78d8aad797b1ababd1a30a2
Blaine Mercado <blainemercado@hotmail.com>
2016-07-25 13:38:14 -0700

    created login and registration

--- apps/poke/urls.py
+++ apps/poke/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name = 'poke_index')
+	url(r'^$', views.index, name = 'poke_index'),
+	url(r'^register$', views.register, name = 'poke_register'),
+	url(r'^login$', views.login, name = 'poke_login')
 ]
--- apps/poke/views.py
+++ apps/poke/views.py
@@ -8,3 +8,36 @@ import re
 # Create your views here.
 def index(request):
 	return render(request, 'poke/index.html')
+
+def register(request):
+	valid = User.userManager.validate(request.POST['username'], request.POST['email'], request.POST['pass1'], request.POST['pass2'])
+	if valid[0]==True:
+		request.session['currUseruserName'] = valid[1].username
+		request.session['currUserID'] = valid[1].id
+		return redirect(reverse('trips_trips'))
+	else:
+		if valid[1] == "name":
+			messages.info(request, "Name must be at least 3 characters")
+		elif valid[1] == "username":
+			messages.info(request, "Username must be at least 3 character")
+		elif valid[1] == "usedname":
+			messages.info(request, "Sorry, that Username has already been taken")
+		elif valid[1] == "email":
+			messages.info(request, "Please enter a valid email")
+		elif valid[1] == "pass1":
+			messages.info(request, "Password must be at least 8 characters")
+		elif valid[1] == "pass2":
+			messages.info(request, "Passwords do not match")
+		return redirect('/')
+
+def login(request):
+	if User.userManager.login(request.POST['username'], request.POST['pass1']) == True:
+		user = User.objects.filter(username=request.POST['username'])
+		request.session['currUseruserName'] = user[0].username
+		request.session['currUserID'] = user[0].id
+		return redirect(reverse('trips_trips'))
+	else:
+		messages.warning(request, 'Username and Password do not match')
+		return redirect(reverse('trips_index'))
+
+
